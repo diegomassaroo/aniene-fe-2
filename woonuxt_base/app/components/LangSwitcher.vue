190cb360c7b7cc1c76cb98ae85d2364625e85c3c
diegomassaroo <diegmassaro@gmail.com>
2024-09-02 21:44:36 +0200

    Test fix langswitch color

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -5,6 +5,17 @@ const switchLocale = (newLocale) => {
   locale.value = newLocale;
   setLocaleCookie(newLocale);
 };
+
+// Computed properties to determine button classes
+const enButtonClass = computed(() => ({
+  'text-blue': locale.value === 'en_US',
+  'text-black': locale.value !== 'en_US'
+}));
+
+const itButtonClass = computed(() => ({
+  'text-blue': locale.value === 'it_IT',
+  'text-black': locale.value !== 'it_IT'
+}));
 </script>
 
 
@@ -12,14 +23,14 @@ const switchLocale = (newLocale) => {
   <div class="relative inline-flex leading-none md:p-3.5 h-fit justify-end md:justify-normal">
     <button 
       @click="switchLocale('en_US')"
-      :class="{ 'text-blue': locale === 'en_US', 'text-black': locale !== 'en_US' }"
+      :class="enButtonClass"
       class="text-small hover:text-blue cursor-pointer leading-0">
       EN
     </button>
     <span class="text-small leading-0">&nbsp;/&nbsp;</span>
     <button 
       @click="switchLocale('it_IT')"
-      :class="{ 'text-blue': locale === 'it_IT', 'text-black': locale !== 'it_IT' }"
+      :class="itButtonClass"
       class="text-small hover:text-blue cursor-pointer leading-0">
       IT
     </button>
