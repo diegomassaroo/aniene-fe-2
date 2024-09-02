4685dd523dcc395ccdd0de1a98255a103f34bf65
diegomassaroo <diegmassaro@gmail.com>
2024-09-02 22:46:42 +0200

    test fix lang color

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -1,27 +1,34 @@
 <script setup lang="ts">
 const { locale, setLocale, setLocaleCookie } = useI18n();
+const currentLocale = ref(locale.value);
+
+onMounted(() => {
+  setLocale(currentLocale.value);
+});
 
 const switchLocale = (newLocale: string) => {
+  currentLocale.value = newLocale;
   setLocale(newLocale);
   setLocaleCookie(newLocale);
 };
 </script>
 
 <template>
-  <div class="langSwitch relative inline-flex leading-none md:p-3.5 h-fit justify-end md:justify-normal">
+  <div v-if="currentLocale" class="langSwitch relative inline-flex leading-none md:p-3.5 h-fit justify-end md:justify-normal">
     <button 
       @click="switchLocale('en_US')"
-      :class="[locale === 'en_US' ? 'active' : '', 'text-small', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[currentLocale === 'en_US' ? 'active' : '', 'text-small', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       EN
     </button>
     <span class="text-small leading-0">&nbsp;/&nbsp;</span>
     <button 
       @click="switchLocale('it_IT')"
-      :class="[locale === 'it_IT' ? 'active' : '', 'text-small', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[currentLocale === 'it_IT' ? 'active' : '', 'text-small', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       IT
     </button>
   </div>
 </template>
+
 
 <style lang="postcss" scoped>
 .langSwitch button.active {
