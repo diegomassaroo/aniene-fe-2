6c9f207fc15a1bab3290d90d1c3cd42fa4274c9d
diegomassaroo <diegmassaro@gmail.com>
2024-09-17 19:16:19 +0200

    deactivating prerender nuxt config

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -1,26 +1,8 @@
 <script setup>
-// Get locale and functions from useI18n
 const { locale, setLocale, setLocaleCookie } = useI18n();
-
-// Use a ref to track the current locale
-const currentLocale = ref(locale.value);
-
-// Log the initial locale when the component is mounted
-onMounted(() => {
-  console.log('Initial locale:', locale.value);
-});
-
-// Watch for changes in locale and update cookie
-watch(currentLocale, (newLocale) => {
-  console.log('Locale changed to:', newLocale);
+const switchLocale = (newLocale) => {
   setLocale(newLocale);
   setLocaleCookie(newLocale);
-});
-
-// Function to switch locale
-const switchLocale = (newLocale) => {
-  console.log('Switching locale to:', newLocale);
-  currentLocale.value = newLocale; // This will trigger the watcher
 };
 </script>
 
@@ -28,13 +10,13 @@ const switchLocale = (newLocale) => {
   <div class="langSwitch relative inline-flex leading-none md:p-3.5 h-fit justify-end md:justify-normal">
     <button 
       @click="switchLocale('en_US')"
-      :class="[currentLocale === 'en_US' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[locale === 'en_US' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       EN
     </button>
     <span class="text-small leading-0">&nbsp;/&nbsp;</span>
     <button 
       @click="switchLocale('it_IT')"
-      :class="[currentLocale === 'it_IT' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[locale === 'it_IT' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       IT
     </button>
   </div>
