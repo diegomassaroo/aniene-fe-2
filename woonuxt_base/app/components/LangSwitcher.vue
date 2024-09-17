2c2d272e5ef593624e0e364aaf88d77c0c375adb
diegomassaroo <diegmassaro@gmail.com>
2024-09-17 19:13:29 +0200

    test fix lang switch

--- woonuxt_base/app/components/LangSwitcher.vue
+++ woonuxt_base/app/components/LangSwitcher.vue
@@ -1,13 +1,26 @@
 <script setup>
-const { locale, setLocale } = useI18n();
-const localeCookie = useCookie('locale');
-if (localeCookie.value) {
-  setLocale(localeCookie.value);
-}
+// Get locale and functions from useI18n
+const { locale, setLocale, setLocaleCookie } = useI18n();
 
-const switchLocale = (newLocale) => {
+// Use a ref to track the current locale
+const currentLocale = ref(locale.value);
+
+// Log the initial locale when the component is mounted
+onMounted(() => {
+  console.log('Initial locale:', locale.value);
+});
+
+// Watch for changes in locale and update cookie
+watch(currentLocale, (newLocale) => {
+  console.log('Locale changed to:', newLocale);
   setLocale(newLocale);
-  localeCookie.value = newLocale;
+  setLocaleCookie(newLocale);
+});
+
+// Function to switch locale
+const switchLocale = (newLocale) => {
+  console.log('Switching locale to:', newLocale);
+  currentLocale.value = newLocale; // This will trigger the watcher
 };
 </script>
 
@@ -15,13 +28,13 @@ const switchLocale = (newLocale) => {
   <div class="langSwitch relative inline-flex leading-none md:p-3.5 h-fit justify-end md:justify-normal">
     <button 
       @click="switchLocale('en_US')"
-      :class="[locale === 'en_US' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[currentLocale === 'en_US' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       EN
     </button>
     <span class="text-small leading-0">&nbsp;/&nbsp;</span>
     <button 
       @click="switchLocale('it_IT')"
-      :class="[locale === 'it_IT' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
+      :class="[currentLocale === 'it_IT' ? 'active' : '', 'text-small', 'text-black', 'hover:text-blue', 'cursor-pointer', 'leading-0']">
       IT
     </button>
   </div>
